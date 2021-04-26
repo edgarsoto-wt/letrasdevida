ed51e7006b0d554d0fb62930b041094137ccfbe7
Edgar Soto <edgar.soto@wundermanthompson.com>
2021-04-25 23:44:16 -0600

    connection with mongo db

---
+++ pages/api/post.js
@@ -0,0 +1,6 @@
+export const getPostData = (postId) => {
+    return {
+        title: `Post title: ${postId}`,
+        description: `Lorem ipusm sit amet sfd ah dfadkajsfa dflajdfa dflajk `
+    }
+}
---
+++ pages/index.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div >
+      <Head>
+        <title>Letras de Vida</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+
+      <footer >
+        <a
+          href="/"
+          rel="noopener noreferrer"
+        >
+          Letras de vida
+        </a>
+      </footer>
+    </div>
+  )
+}
